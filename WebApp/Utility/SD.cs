5d1ac9763fa9499f8f93e1c03ceadd470f03c210
Andrey Vlasov <lion32211@yandex.ru>
2025-02-06 22:46:26 +0100

    Init commint. Created Coupon API

---
+++ WebApp/Utility/SD.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApp.Utility
+{
+    public class SD
+    {
+        public enum ApiType
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
