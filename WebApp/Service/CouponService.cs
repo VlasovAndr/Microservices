e0e8e5fdd6dd8a0032f0a08c95944a71e4e20921
Andrey Vlasov <lion32211@yandex.ru>
2025-02-08 15:38:26 +0100

    Added CouponService for API usage in WebApp project. Soma classes was copied from API project

---
+++ WebApp/Service/CouponService.cs
@@ -0,0 +1,73 @@
+﻿
+using WebApp.Models;
+using WebApp.Service.IService;
+using WebApp.Utility;
+
+namespace WebApp.Service
+{
+    public class CouponService : ICouponService
+    {
+        private readonly IBaseService _baseService;
+
+        public CouponService(IBaseService baseService)
+        {
+            _baseService = baseService;
+        }
+
+        public async Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.POST,
+                Data = couponDto,
+                Url = SD.CouponAPIBase + "/api/coupon/"
+            });
+        }
+
+        public async Task<ResponseDto?> DeleteCouponAsync(int id)
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.DELETE,
+                Url = SD.CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> GetAllCouponsAsync()
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/coupon"
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponAsync(string couponCode)
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/coupon/GetByCode/" + couponCode
+            });
+        }
+
+        public async Task<ResponseDto?> GetCouponsByIdAsync(int id)
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.GET,
+                Url = SD.CouponAPIBase + "/api/coupon/" + id
+            });
+        }
+
+        public async Task<ResponseDto?> UpdateAllCouponAsync(CouponDto couponDto)
+        {
+            return await _baseService.SendAsunc(new RequestDto()
+            {
+                ApiType = SD.ApiType.PUT,
+                Data = couponDto,
+                Url = SD.CouponAPIBase + "/api/coupon/"
+            });
+        }
+    }
+}
---
+++ WebApp/Service/IService/ICouponService.cs
@@ -0,0 +1,14 @@
+﻿using WebApp.Models;
+
+namespace WebApp.Service.IService
+{
+    public interface ICouponService
+    {
+        Task<ResponseDto?> GetCouponAsync(string couponCode);
+        Task<ResponseDto?> GetAllCouponsAsync();
+        Task<ResponseDto?> GetCouponsByIdAsync(int id);
+        Task<ResponseDto?> CreateCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> UpdateAllCouponAsync(CouponDto couponDto);
+        Task<ResponseDto?> DeleteCouponAsync(int id);
+    }
+}
