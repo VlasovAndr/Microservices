bfbc3a461dad01145b1655e8311135710bd12960
Andrey Vlasov <lion32211@yandex.ru>
2025-02-11 22:01:01 +0100

    Added Custom Properties to User table(Based on IdentityUser)

--- AuthAPI/Program.cs
+++ AuthAPI/Program.cs
@@ -1,4 +1,5 @@
 using AuthAPI.Data;
+using AuthAPI.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,7 +10,7 @@ builder.Services.AddDbContext<AppDbContext>(option =>
 {
 	option.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>().AddEntityFrameworkStores<AppDbContext>()
 	.AddDefaultTokenProviders();
 
 builder.Services.AddControllers();
