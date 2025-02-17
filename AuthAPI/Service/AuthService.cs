2f5f1c7877aea0f00968beb498ba8cac13a78885
Andrey Vlasov <lion32211@yandex.ru>
2025-02-17 21:09:11 +0100

    Added IAuth Service

---
+++ AuthAPI/Service/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using AuthAPI.Data;
+using AuthAPI.Models;
+using AuthAPI.Models.Dto;
+using AuthAPI.Service.IService;
+using Microsoft.AspNetCore.Identity;
+
+namespace AuthAPI.Service
+{
+	public class AuthService : IAuthService
+	{
+		private readonly AppDbContext _db;
+		private readonly UserManager<ApplicationUser> _userManager;
+		private readonly RoleManager<IdentityRole> _roleManager;
+
+		public AuthService(AppDbContext db, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+		{
+			_db = db;
+			_userManager = userManager;
+			_roleManager = roleManager;
+		}
+
+		public Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+		{
+			throw new NotImplementedException();
+		}
+
+		public Task<UserDto> Register(RegistrationRequestDto registrationRequestDto)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
---
+++ AuthAPI/Service/IService/IAuthService.cs
@@ -0,0 +1,10 @@
+﻿using AuthAPI.Models.Dto;
+
+namespace AuthAPI.Service.IService
+{
+	public interface IAuthService
+	{
+		Task<UserDto> Register(RegistrationRequestDto registrationRequestDto);
+		Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+	}
+}
