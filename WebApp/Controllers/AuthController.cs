1ccf1c210c9df085bbbf171b8240d8d994517a18
Andrey Vlasov <lion32211@yandex.ru>
2025-02-26 19:14:49 +0100

    Added Auth Controller in Web Project

---
+++ WebApp/Controllers/AuthController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+using WebApp.Service.IService;
+
+namespace WebApp.Controllers
+{
+	public class AuthController : Controller
+	{
+		private IAuthService _authService;
+
+		public AuthController(IAuthService authService)
+		{
+			_authService = authService;
+		}
+
+		[HttpGet]
+
+		public IActionResult Login()
+		{
+			LoginRequestDto loginRequestDto = new LoginRequestDto();
+			return View(loginRequestDto);
+		}
+
+		[HttpGet]
+
+		public IActionResult Register()
+		{
+			return View();
+		}
+
+		public IActionResult Logout()
+		{
+			return View();
+		}
+
+	}
+}
