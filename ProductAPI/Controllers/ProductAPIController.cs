af8edde9ba9150cc265bc76f51cdee0e859bea40
Andrey Vlasov <lion32211@yandex.ru>
2025-03-05 11:48:23 +0100

    ProductAPI. CRUD functionality Product API

---
+++ ProductAPI/Controllers/ProductAPIController.cs
@@ -0,0 +1,128 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using ProductAPI.Data;
+using ProductAPI.Models;
+using ProductAPI.Models.Dto;
+
+namespace ProductAPI.Controllers
+{
+	[Route("api/product")]
+	[ApiController]
+	public class ProductAPIController : ControllerBase
+	{
+		private readonly AppDbContext _db;
+		private ResponseDto _response;
+		private IMapper _mapper;
+
+		public ProductAPIController(AppDbContext db, IMapper mapper)
+		{
+			_db = db;
+			_response = new ResponseDto();
+			_mapper = mapper;
+		}
+
+		[HttpGet]
+		public ResponseDto Get()
+		{
+			try
+			{
+				IEnumerable<Product> objList = _db.Products.ToList();
+				_response.Result = _mapper.Map<IEnumerable<ProductDto>>(objList);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpGet]
+		[Route("{id:int}")]
+		public ResponseDto Get(int id)
+		{
+			try
+			{
+				Product obj = _db.Products.First(x => x.ProductId == id);
+				_response.Result = _mapper.Map<ProductDto>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpGet]
+		[Route("GetByName{name}")]
+		public ResponseDto Get(string name)
+		{
+			try
+			{
+				Product obj = _db.Products.First(x => x.Name.ToLower() == name.ToLower());
+				_response.Result = _mapper.Map<ProductDto>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpPost]
+		public ResponseDto Post([FromBody] ProductDto productDto)
+		{
+			try
+			{
+				Product obj = _mapper.Map<Product>(productDto);
+				_db.Products.Add(obj);
+				_db.SaveChanges();
+				_response.Result = _mapper.Map<ProductDto>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpPut]
+		public ResponseDto Put([FromBody] ProductDto productDto)
+		{
+			try
+			{
+				Product obj = _mapper.Map<Product>(productDto);
+				_db.Products.Update(obj);
+				_db.SaveChanges();
+				_response.Result = _mapper.Map<ProductDto>(obj);
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+
+		[HttpDelete]
+		[Route("{id:int}")]
+		public ResponseDto Delete(int id)
+		{
+			try
+			{
+				Product obj = _db.Products.First(x => x.ProductId == id);
+				_db.Products.Remove(obj);
+				_db.SaveChanges();
+			}
+			catch (Exception ex)
+			{
+				_response.IsSuccess = false;
+				_response.Message = ex.Message;
+			}
+			return _response;
+		}
+	}
+}
