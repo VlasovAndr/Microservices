6130617925ee4db1da0c32a32191f0579e7ea6b0
Andrey Vlasov <lion32211@yandex.ru>
2025-04-01 20:59:49 +0200

    ShoppingCartAPI. Added RemoveCart end point for CartAPIController

--- ShoppingCartAPI/Models/CartDetails.cs
+++ ShoppingCartAPI/Models/CartDetails.cs
@@ -8,8 +8,8 @@ public class CartDetails
 {
     [Key]
     public int CartDetailsId { get; set; }
-    public int CartHeaderId { get; set; }
     [ForeignKey("CartHeaderId")]
+    public int CartHeaderId { get; set; }
     public CartHeader CartHeader { get; set; }
     public int ProductId { get; set; }
     [NotMapped]
