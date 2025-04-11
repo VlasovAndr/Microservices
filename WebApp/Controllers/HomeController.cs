da5113f1c6c666e7bd25c8e76d4909e7d209ca39
Andrey Vlasov <lion32211@yandex.ru>
2025-04-11 20:09:21 +0200

    WebApp. Added functionality to Add items to Shopping Cart

--- WebApp/Controllers/HomeController.cs
+++ WebApp/Controllers/HomeController.cs
@@ -1,3 +1,4 @@
+using Duende.IdentityModel;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
@@ -7,61 +8,100 @@ using WebApp.Service.IService;
 
 namespace WebApp.Controllers
 {
-    public class HomeController : Controller
-    {
-        private readonly IProductService _productService;
+	public class HomeController : Controller
+	{
+		private readonly IProductService _productService;
+		private readonly IShoppingCartService _shoppingCartService;
 
-        public HomeController(IProductService productService)
-        {
-            _productService = productService;
-        }
+		public HomeController(IProductService productService, IShoppingCartService shoppingCartService)
+		{
+			_productService = productService;
+			_shoppingCartService = shoppingCartService;
+		}
 
-        public async Task<IActionResult> Index()
-        {
-            List<ProductDto>? list = new List<ProductDto>();
+		public async Task<IActionResult> Index()
+		{
+			List<ProductDto>? list = new List<ProductDto>();
 
-            ResponseDto? response = await _productService.GetAllProductsAsync();
+			ResponseDto? response = await _productService.GetAllProductsAsync();
 
-            if (response != null && response.IsSuccess)
-            {
-                list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
-            }
-            else
-            {
-                TempData["error"] = response?.Message;
-            }
+			if (response != null && response.IsSuccess)
+			{
+				list = JsonConvert.DeserializeObject<List<ProductDto>>(Convert.ToString(response.Result));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
 
-            return View(list);
-        }
+			return View(list);
+		}
 
-        [Authorize]
-        public async Task<IActionResult> ProductDetails(int productId)
-        {
-           var product = new ProductDto();
+		[Authorize]
+		public async Task<IActionResult> ProductDetails(int productId)
+		{
+			var product = new ProductDto();
 
-            ResponseDto? response = await _productService.GetProductByIdAsync(productId);
+			ResponseDto? response = await _productService.GetProductByIdAsync(productId);
 
-            if (response != null && response.IsSuccess)
-            {
-                product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
-            }
-            else
-            {
-                TempData["error"] = response?.Message;
-            }
+			if (response != null && response.IsSuccess)
+			{
+				product = JsonConvert.DeserializeObject<ProductDto>(Convert.ToString(response.Result));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
 
-            return View(product);
-        }
+			return View(product);
+		}
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
+		[Authorize]
+		[HttpPost]
+		[ActionName("ProductDetails")]
+		public async Task<IActionResult> ProductDetails(ProductDto productDto)
+		{
+			CartDto cartDto = new CartDto()
+			{
+				CartHeader = new CartHeaderDto
+				{
+					UserId = User.Claims.Where(u => u.Type == JwtClaimTypes.Subject)?.FirstOrDefault()?.Value
+				}
+			};
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+			CartDetailsDto cartDetailsDto = new CartDetailsDto()
+			{
+				Count = productDto.Count,
+				ProductId = productDto.ProductId
+			};
+
+			List<CartDetailsDto> cartDetailsDtos = new List<CartDetailsDto>() { cartDetailsDto };
+			cartDto.CartDetails = cartDetailsDtos;
+
+			ResponseDto? response = await _shoppingCartService.UpsertCartAsync(cartDto);
+
+			if (response != null && response.IsSuccess)
+			{
+				TempData["success"] = "Item has been added to the Shopping cart";
+				return RedirectToAction(nameof(Index));
+			}
+			else
+			{
+				TempData["error"] = response?.Message;
+			}
+
+			return View(productDto);
+		}
+
+		public IActionResult Privacy()
+		{
+			return View();
+		}
+
+		[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+		public IActionResult Error()
+		{
+			return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+		}
+	}
 }
