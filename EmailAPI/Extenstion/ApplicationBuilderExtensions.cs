b397bbac72183837150201271c7d9bc3e9627378
Andrey Vlasov <lion32211@yandex.ru>
2025-05-07 19:10:32 +0200

    EmailAPI. Register Service Bus Consumer on App start

---
+++ EmailAPI/Extenstion/ApplicationBuilderExtensions.cs
@@ -0,0 +1,29 @@
+﻿using EmailAPI.Messaging;
+
+namespace EmailAPI.Extenstion;
+
+public static class ApplicationBuilderExtensions
+{
+	private static IAzureServiceBusConsumer ServiceBusConsumer { get; set; }
+
+	public static IApplicationBuilder UseAzureServiceBusConsumer(this IApplicationBuilder app)
+	{
+		ServiceBusConsumer = app.ApplicationServices.GetService<IAzureServiceBusConsumer>();
+		var hostApplicationLife = app.ApplicationServices.GetService<IHostApplicationLifetime>();
+
+		hostApplicationLife.ApplicationStarted.Register(OnStart);
+		hostApplicationLife.ApplicationStopped.Register(OnStop);
+
+		return app;
+	}
+
+	private static void OnStop()
+	{
+		ServiceBusConsumer.Stop();
+	}
+
+	private static void OnStart()
+	{
+		ServiceBusConsumer.Start();
+	}
+}
