2c67b23923c067cc1492f820855bb38987937500
Andrey Vlasov <lion32211@yandex.ru>
2025-05-18 17:43:47 +0200

    OrderAPI. Created OrderAPI project, adde DTOs

---
+++ OrderAPI/Models/Dto/OrderDetailsDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OrderAPI.Models.Dto;
+
+public class OrderDetailsDto
+{
+	[Key]
+	public int OrderDetailsId { get; set; }
+	public int CartHeaderId { get; set; }
+	public int ProductId { get; set; }
+	public ProductDto? Product { get; set; }
+	public int Count { get; set; }
+	public string ProductName { get; set; }
+	public double Price { get; set; }
+}
